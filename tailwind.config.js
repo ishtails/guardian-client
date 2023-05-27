23047dae0754d7380ce6603ed6f88ac8b9b370b5
Styles <bcs_2021035@iiitm.ac.in>
2023-05-27 19:55:16 +0530

    added figma styles to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    "fontFamily": {
+      "lexend": "Lexend",
+      sans: ["Inter", 'sans-serif']
+     },
+
+    extend: {
+      "colors": {
+        "primary": "#0ea5e9",
+        "placeholder": "#667085",
+        "amber-light": "#fffbeb",
+        "amber-dark": "#78350f",
+        "yellow": "#ffbd00",
+        "brown": "#241b00"
+       },
+
+       "fontSize": {
+        "p14": "0.875rem",
+        "p16": "1rem",
+        "p18": "1.125rem",
+        "p20": "1.25rem",
+        "h24": "1.5rem",
+        "h28": "1.75rem",
+        "h32": "2rem",
+        "h36": "2.25rem"
+       },
+
+       "boxShadow": {
+        "card-shadow": "0px 0px 0px 0px rgba(38,50,56,0.1), -1px 1px 3px 0px rgba(38,50,56,0.1), -2px 4px 5px 0px rgba(38,50,56,0.09), -5px 9px 6px 0px rgba(38,50,56,0.05), -9px 17px 8px 0px rgba(38,50,56,0.01), -15px 26px 8px 0px rgba(38,50,56,0)"
+       },
+    },
   },
   plugins: [],
 }
