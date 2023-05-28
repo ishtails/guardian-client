ed074f034a885cc94799734aefe11a9b0d150320
Aneeka Mangal <aneekamangal@gmail.com>
2023-05-28 18:37:59 +0530

    mobile responsive

--- src/components/LoginComp.tsx
+++ src/components/LoginComp.tsx
@@ -9,20 +9,18 @@ const LoginComp = (props) => {
     setIsPasswordVisible((prevState) => !prevState);
   }
   return (
-    <div className="flex flex-col w-[50%]">
-          <div className="flex space-x-4">
-            <img src={logo} className="w-[32px]" />
-            <div className="hidden lg:flex flex-row font-lexend text-h28 text-primary">Guar <div className="font-lexend font-bold text-h28">dian</div></div>
-          </div>
-          <div className="flex flex-row h-[650px] justify-center items-center">
-            <div className="bg-white rounded-xl shadow-card-shadow space-y-4 p-5 h-[450px] w-[400px]">
-              <span className="font-lexend font-bold text-h36">{props.name} Login</span>
-              <p className="text-[#667085] text-h16">Please fill your detail to access your account.</p>
-              <div>
+    <div className="flex flex-col">
+          {/* <div className="flex flex-row h-[650px] justify-center items-center">
+            <div className="bg-white rounded-xl shadow-card-shadow space-y-4 p-5 h-[450px] w-[400px]"> */}
+            <div className="py-2">
+                <span className="font-lexend font-bold text-h36 sm:text-h32">{props.name} Login</span>
+                <p className="text-[#667085] text-h16 sm:text-h14">Please fill your detail to access your account.</p>
+              </div>
+              <div className="py-2">
                 <h1 className="text-[#344054] text-h14">Institute Email</h1>
                 <input type="text" placeholder="admin@iiitm.ac.in" className="border-2 rounded-lg border-[#D0D5DD] text-[#667085] text-h16 p-1 w-full "/>
               </div>
-              <div>
+              <div className="py-2">
                 <h1 className="text-[#344054] text-h14">Password</h1>
                 <div className="relative">
                 <input type={isPasswordVisible ? "text" : "password"} placeholder="Password" className="border-2 rounded-lg border-[#D0D5DD] text-[#667085] text-h16 p-1 w-full"/>
@@ -66,21 +64,23 @@ const LoginComp = (props) => {
                 </button>
                 </div>
               </div>
-              <div className="text-h14 flex space-x-24">
+              <div className="text-h14 flex lg:space-x-24 py-2 md:space-x-16 sm:space-x-12">
                 <div className="flex space-x-1">
                   <input id="default-checkbox" type="checkbox" value="" className="text-[#D0D5DD]"></input>
                   <h1 className="text-[#344054] ">Remember me</h1>
                 </div>
                 <a href="" className="text-[#0EA5E9]">Forgot Password?</a>
               </div>
-              <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg">Sign In</button>
-              <div className="flex flex-col items-center text-decoration-line: underline">
+              <div className="py-2">
+                <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg">Sign In</button>
+              </div>
+              <div className="flex flex-col items-center text-decoration-line: underline py-2">
                 <a href="" className="text-[#104A70] ">Student? Click Here</a>
                 <a href="" className="text-[#104A70]">Security Guard? Click Here</a>
               </div>
             </div>
-          </div>
-        </div>
+        //   </div>
+        // </div>
   )
 }
 
--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,5 +1,6 @@
 import logo from "../../assets/icons/logo.svg"
 import login from "../../assets/illustrations/login.svg"
+import login2 from "../../assets/illustrations/login2.svg"
 import { useState } from "react";
 import LoginComp from "../../components/LoginComp";
 
@@ -7,15 +8,36 @@ const adminLogin = () => {
   
   return (
     <>
-    <div className="bg-[#FCFFFF] m-6 space-y-8 h-[100vh]">
+    {/* desktop */}
+    <div className="bg-[#FCFFFF] m-6 space-y-8 h-[100vh] hidden sm:flex flex-col">
       <div className="flex space-x-6">
-        <LoginComp name="Admin"/>
+      <div className="flex flex-col w-[50%]">
+          <div className="flex space-x-4">
+            <img src={logo} className="w-[32px]" />
+            <div className="hidden lg:flex flex-row font-lexend text-h28 text-primary">Guar <div className="font-lexend font-bold text-h28">dian</div></div>
+          </div>
+          <div className="flex flex-row h-[650px] justify-center items-center">
+            <div className="bg-white rounded-xl shadow-card-shadow space-y-4 p-5 h-[450px] w-[400px]">
+          <LoginComp name="Admin"/>  
+          </div>
+          </div>
+        </div>
         <div className="flex flex-col bg-[#F0F9FF] rounded-xl shadow-card-shadow w-[50%] space-y-4 p-5 items-center h-[700px]">
           <img src={login} alt="" className="h-[700.17px] w-[731.45px]"/>
         </div>
       </div>
     </div>
-    
+    {/* mobile */}
+    <div className="bg-[#FCFFFF] m-6 space-y-8 h-[100vh] w-[375px] flex flex-col sm:hidden">
+      <div className="flex flex-col space-x-6">
+        <img src={login2} alt=""/>
+        <LoginComp name="Admin"/>
+        <hr className="h-0.5 mx-auto my-4 border-0 rounded bg-[#E0E0E0] justify-center items-center"/>
+        <div className="flex flex-col justify-center items-center">
+          <img src={logo} className="w-[32px] " />
+        </div>
+      </div>
+    </div>
   </>
   )
 }
