30f914fecd678f99cb9ffdc23d463436a1203ffd
Styles <bcs_2021035@iiitm.ac.in>
2023-05-29 13:08:35 +0530

    routes updation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,15 @@ import AdminChangePassword from "./pages/admin/changePassword";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<AdminLogin />} />
+      <Route path="/" element={<div>Student Login</div>} />
+      <Route path="/home" element={<div>Student Dashboard</div>} />
+      <Route path="/changepass" element={<div>Student Change Password</div>} />
+
+      <Route path="/security" element={<div>Security Login</div>} />
+      <Route path="/security/home" element={<div>Security Dashboard</div>} />
+      <Route path="/security/changepass" element={<div>Security Dashboard</div>} />
+
+      <Route path="/admin" element={<AdminLogin />} />
       <Route path="/admin/home" element={<AdminDashboard />} />
       <Route path="/admin/changepass" element={<AdminChangePassword />} />
 
