6c326fcf38fb4f6884904e8ecc0cf31be4524e4d
Styles <bcs_2021035@iiitm.ac.in>
2023-05-29 18:01:24 +0530

    added flowbite & preline tailwind plugins

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import('preline')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
