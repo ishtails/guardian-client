59cdefe781650e1286638fe1dc375221c2519e10
Aneeka Mangal <aneekamangal@gmail.com>
2023-06-14 00:47:27 +0530

    modified a bit

--- src/pages/security/closingDialogue.tsx
+++ src/pages/security/closingDialogue.tsx
@@ -22,7 +22,7 @@ export default function Example() {
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="fixed inset-0 z-10 overflow-y-auto ">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
@@ -36,13 +36,14 @@ export default function Example() {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                       <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Sure?
+                    </div> */}
+                    <div className="mt-3 font-lexend sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="text-p20 font-semibold leading-6">
+                        Aneeka Mangal
                       </Dialog.Title>
+                    <p className="text-p16">2021BCS-012</p>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
                           Are you sure you want to close the entry?
