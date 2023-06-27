ef177a311f63fbf4edd0d7d6c8b7a1fb02e55e36
Aneeka Mangal <aneekamangal@gmail.com>
2023-06-27 17:09:48 +0530

    minor ui fixes

--- src/pages/student/update.tsx
+++ src/pages/student/update.tsx
@@ -20,95 +20,111 @@ const updateInfo: React.FC<> = () => {
     
     <div className=" bg-[#FCFFFF] relative">
         {/* Logo */}
-      <div className="hidden z-20 md:flex p-5 lg:px-5 absolute space-x-3 left-2 top-0">
-        <img src={logo} className="w-[32px]" />
-        <div className="flex flex-row font-lexend text-h28 text-primary">
-          Guar
-          <span className="font-lexend font-bold text-h28">dian</span>
+        <div className="hidden z-20 md:flex p-5 lg:px-5 absolute space-x-3 left-2 top-0">
+            <img src={logo} className="w-[32px]" />
+            <div className="flex flex-row font-lexend text-h28 text-primary">
+                Guar
+                <span className="font-lexend font-bold text-h28">dian</span>
+            </div>
         </div>
-      </div>
         
-      {/* Background Gradient */}
-      <div className="px-6 h-screen hidden md:flex flex-col relative justify-center">
-        <img
-          src={blur_cyan}
-          alt=""
-          className="absolute z-0 top-[3%] left-[26%] w-[400px]"
-        />
+        {/* Background Gradient */}
+        <div className="px-6 h-screen hidden md:flex flex-col relative justify-center">
+            <img
+            src={blur_cyan}
+            alt=""
+            className="absolute z-0 top-[3%] left-[26%] w-[400px]"
+            />
 
-      <div className="flex flex-col z-0 relative">
-        <div className="grid grid-cols-2 items-center m-auto w-full">
-            {/* Login Component */}
+            <div className="flex flex-col z-0 relative">
+                <div className="grid grid-cols-2 items-center m-auto w-full">
+                    {/* Login Component */}
 
-            <div className="grid justify-center items-center h-full w-full">
-              <div className="scale-75 xl:scale-100 z-10 backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 p-6 w-[400px]">
-                {/* Header */}
-                <span className="font-lexend font-bold text-h36 sm:text-h32">
-                    Update Profile
-                </span>
-                <p className="text-[#667085] text-h16 sm:text-h14">Change your information below</p>
+                    <div className="grid justify-center items-center h-full w-full">
+                        <div className="scale-75 xl:scale-100 z-10 backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 p-6 w-[400px]">
+                            {/* Header */}
+                            <span className="font-lexend font-bold text-h36 sm:text-h32">
+                                Update Profile
+                            </span>
+                            <p className="text-[#667085] text-h16 sm:text-h14">Change your information below</p>
 
 
-                {/* Input Fields */}
-                <InputField
-                label="Full Name"
-                placeholder=""
-                isPassword={false}
-                />
-                <InputField
-                label="Phone"
-                placeholder=""
-                isPassword={false}
-                />
-                <InputField
-                label="Hostel"
-                placeholder=""
-                isPassword={false}
-                />
-                <InputField
-                label="Room"
-                placeholder=""
-                isPassword={false}
-                />
-                {/* Select ID card image*/}
-                <div>
-                    <div {...getRootProps() } className={`dropzone ${isDragActive ? 'active' : ''}`}>
-                    <input {...getInputProps()} accept="image/*"/>
-                    {idCard ? (
-                        <p>Image selected : {idCard.name}</p>
-                    ) : (
-                        <p className="cursor-pointer">Drag and drop your identity card image here, or click to select a file</p>
-                    )}
+                            {/* Input Fields */}
+                            <InputField
+                            label="Full Name"
+                            placeholder=""
+                            isPassword={false}
+                            />
+                            <InputField
+                            label="Phone"
+                            placeholder=""
+                            isPassword={false}
+                            />
+                            <InputField
+                            label="Hostel"
+                            placeholder=""
+                            isPassword={false}
+                            />
+                            <InputField
+                            label="Room"
+                            placeholder=""
+                            isPassword={false}
+                            />
+                            {/* Select ID card image*/}
+                            <div>
+                                <div {...getRootProps() } className={`dropzone ${isDragActive ? 'active' : ''}`}>
+                                    <input {...getInputProps()} accept="image/*"/>
+                                    {idCard ? (
+                                        <p>Image selected : {idCard.name}</p>
+                                    ) : (
+                                        <p className="cursor-pointer">Drag and drop your identity card image here, or click to select a file</p>
+                                    )}
+                                </div>
+                            </div>
+                            {/* Submit Button */}
+                            <div className="pt-1">
+                                <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg hover:bg-sky-400 transition-all font-semibold">
+                                    Submit
+                                </button>
+                            </div>
+
+                            {/* Footer */}
+                            <Link
+                            to="/student/home"
+                            className="text-[#0EA5E9] text-center font-medium hover:text-sky-600 transition hover:underline underline-offset-1"
+                            >
+                            Back to Home
+                            </Link>
+                        </div>
+                    </div>
+                    {/* Illustration */}
+                    <div className=" z-10 grid justify-center backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 m-5 p-5 items-center">
+                        <img
+                            src={update}
+                            alt=""
+                            className="py-14 max-h-[720px]"
+                        />
                     </div>
                 </div>
-                {/* Submit Button */}
-                <div className="pt-1">
-                <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg hover:bg-sky-400 transition-all font-semibold">
-                    Submit
-                </button>
-                </div>
-
-                {/* Footer */}
-                <Link
-                to="/student/home"
-                className="text-[#0EA5E9] text-center font-medium hover:text-sky-600 transition hover:underline underline-offset-1"
-                >
-                Back to Home
-                </Link>
-              </div>
             </div>
-            {/* Illustration */}
-            <div className=" z-10 grid justify-center backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 m-5 p-5 items-center">
-              <img
-                src={update}
-                alt=""
-                className="py-14 max-h-[720px]"
-              />
-            </div>
-          </div>
         </div>
-      </div>
+
+        {/* Mobile Screen */}
+        <div className="flex flex-col md:hidden px-5 h-screen">
+            <img src={update} className="my-4" />
+
+            <props.InputField />
+
+            <footer className="mt-auto bottom-0">
+            <hr className="my-2" />
+            <div className="flex flex-col justify-center items-center mb-2">
+                <img src={logo} className="w-[32px] " />
+            </div>
+            </footer>
+        </div>
     </div>
+
+    
   );
 };
 
