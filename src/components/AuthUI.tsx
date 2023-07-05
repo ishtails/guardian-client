62e9488d19bfdfc4fd9265286c4f529e4fa23217
Styles <bcs_2021035@iiitm.ac.in>
2023-07-05 17:42:25 +0530

    AuthUI fixed!

--- src/components/AuthUI.tsx
+++ src/components/AuthUI.tsx
@@ -1,5 +1,5 @@
-import logo from "./../assets/icons/logo.svg";
 import blur_cyan from "./../assets/blur-cyan.svg";
+import logo from "./../assets/icons/logo.svg";
 
 const authUI = (props: {
   InputField: any;
@@ -8,32 +8,25 @@ const authUI = (props: {
 }) => {
   return (
     <div className="bg-[#FCFFFF] relative">
-      {/* Logo NOT LOOKING GOOD HERE */}
-      {/* <div className="hidden z-20 md:flex p-5 lg:px-5 absolute space-x-3 left-2 top-0">
-        <img src={logo} className="w-[32px]" />
-        <div className="flex flex-row font-lexend text-h28 text-primary">
-          Guar
-          <span className="font-lexend font-bold text-h28">dian</span>
-        </div>
-      </div> */}
-
-      <div className="px-6 h-screen hidden md:flex flex-col relative justify-center">
+      <div className="md:px-6 md:h-screen flex flex-col relative md:justify-center">
         <img
           src={blur_cyan}
           alt=""
-          className="absolute z-0 top-[3%] left-[26%] w-[400px]"
+          className="hidden md:block absolute z-0 top-[3%] left-[26%] w-[400px]"
         />
 
-        <div className="grid grid-cols-2 items-center h-screen m-auto w-full">
+        <img src={props.illustration_2} className="md:hidden my-4" />
+
+        <div className="md:grid md:grid-cols-2 flex items-center md:h-screen m-auto w-full">
           {/* Login Component */}
-          <div className="grid justify-center items-center w-full">
-            <div className="my-10 z-10 backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 p-6 w-[22rem] lg:w-[25rem]">
+          <div className=" md:grid w-screen justify-center items-center md:w-full">
+            <div className=" md:my-10 z-10 md:backdrop-blur-xl md:bg-[#F0F9FF]/50 md:rounded-xl md:shadow-card-shadow space-y-4 p-6 w-full md:w-[22rem] lg:w-[25rem]">
               <props.InputField />
             </div>
           </div>
 
           {/* Illustration */}
-          <div className="z-10 grid justify-center backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 m-5 p-5 items-center">
+          <div className="z-10 hidden md:grid justify-center backdrop-blur-xl bg-[#F0F9FF]/50 rounded-xl shadow-card-shadow space-y-4 m-5 p-5 items-center">
             <img
               src={props.illustration_1}
               alt=""
@@ -41,15 +34,8 @@ const authUI = (props: {
             />
           </div>
         </div>
-      </div>
 
-      {/* Mobile Screen */}
-      <div className="flex flex-col md:hidden px-5 h-screen">
-        <img src={props.illustration_2} className="my-4" />
-
-        <props.InputField />
-
-        <footer className="mt-auto bottom-0">
+        <footer className="md:hidden mt-auto w-screen bottom-0">
           <hr className="my-2" />
           <div className="flex flex-col justify-center items-center mb-2">
             <img src={logo} className="w-[32px] " />
