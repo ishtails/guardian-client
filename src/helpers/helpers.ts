dc6707139a4386bcc4d570937b516ba69c125995
Styles <bcs_2021035@iiitm.ac.in>
2023-07-06 00:42:55 +0530

    updateInfo done!

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -22,3 +22,18 @@ export const getLocation = () => {
         return (location)
     }
 };
+
+// Conver to TitleCase
+export const ToTitleCase = (value:string) => {
+  const words = value.split(' ');
+
+  const capitalizedWords = words.map((word) => {
+    const lowercaseWord = word.toLowerCase();
+    const firstLetterCapitalized = lowercaseWord.charAt(0).toUpperCase();
+    const restOfWord = lowercaseWord.slice(1);
+
+    return firstLetterCapitalized + restOfWord;
+  });
+
+  return capitalizedWords.join(' ');
+};
