4f8e222c37adb236d8d043cde55c8de44aee5e8a
Styles <bcs_2021035@iiitm.ac.in>
2023-07-07 02:25:09 +0530

    Update fetchHook.ts

--- src/helpers/fetchHook.ts
+++ src/helpers/fetchHook.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useUserStore } from "../store/store";
 
 const useFetch = (query: string) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
+  const {user, setUser,  isLoading, setIsLoading} = useUserStore();
   useEffect(() => {
-    if (!query) return;
+    if (!query) {
+      setIsLoading(false);
+      return;
+    };
 
     const fetchData = async () => {
-      setIsLoading(true);
       try {
         const response = await axios.get(query);
-        setData(response.data);
+        setUser(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -22,7 +23,7 @@ const useFetch = (query: string) => {
     fetchData();
   }, [query]);
 
-  return {data, isLoading};
+  return {user, isLoading};
 };
 
 export default useFetch;
