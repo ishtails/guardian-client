722db45cfd494b2e532aea46f74e7dc3bc748cbf
Styles <bcs_2021035@iiitm.ac.in>
2023-07-07 03:45:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import axios from "axios";
 import MainRouter from "./routes/router";
 import { Toaster } from "react-hot-toast";
-import useFetchProfile from "./helpers/fetchHook";
 
 axios.defaults.baseURL = "http://localhost:8000/api";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const App = () => {
-  useFetchProfile('/profile');
-
   return (
     <div>
       <MainRouter />
