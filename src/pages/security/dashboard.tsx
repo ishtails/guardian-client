478836dd5fba755081c0b05d7c15b86346395875
Styles <bcs_2021035@iiitm.ac.in>
2023-07-08 05:49:36 +0530

    security dashboard updated

--- src/pages/security/dashboard.tsx
+++ src/pages/security/dashboard.tsx
@@ -4,18 +4,22 @@ import Searchbar from "../../components/Searchbar";
 import Table from "../../components/Table";
 import logo from "../../assets/icons/logo.svg";
 import React from "react";
-import { Link } from "react-router-dom";
 import useFetchOutings from "../../helpers/fetchOutingHook";
 import { LuClipboardCheck } from "react-icons/lu";
 import { useOutingStore } from "../../store/store";
 import moment from "moment";
+import { useEffect } from "react";
 
 type TableColumn = any;
 type TableRow = any;
 
 const securityDashboard: React.FC = () => {
-  useFetchOutings("/outings", { isOpen: true });
-  const { outing, isLoading } = useOutingStore();
+  useFetchOutings("/outings", {});
+  const { outing, isLoading, filter, setFilter } = useOutingStore();
+  
+  useEffect(() => {
+    setFilter({ ...filter, isOpen: true });
+  }, [isLoading]);
 
   const columns: TableColumn[] = [
     "Roll No",
@@ -25,7 +29,7 @@ const securityDashboard: React.FC = () => {
     "Out Time",
     "In Time",
     "Reason",
-    "Status",
+    `${filter?.isOpen ? "Status" : "Late By"}`,
   ];
   const values: TableRow[] = [];
 
@@ -56,6 +60,11 @@ const securityDashboard: React.FC = () => {
     });
   }
 
+  const dropDownNavSecurity = [
+    { href: "/changepass", label: "Change Password" },
+    { href: "/logout", label: "Sign Out" },
+  ];
+
   return (
     <div className="bg-[#FCFFFF] h-screen">
       {/* Desktop */}
@@ -67,7 +76,7 @@ const securityDashboard: React.FC = () => {
           <div className="overflow-auto mb-5 flex flex-col bg-white rounded-xl shadow-card-shadow w-full space-y-4 p-5">
             <span className="flex items-center justify-between ">
               <h1 className="font-lexend font-bold text-h24 mx-4">
-                Open Entries
+                {filter?.isOpen ? "Open Entries" : "Closed Entries"}
               </h1>
               <div className=" px-2 py-1 rounded-lg text-sm font-medium">
                 {moment().format("YYYY-MM-DD")}
@@ -82,19 +91,35 @@ const securityDashboard: React.FC = () => {
       <div className="md:hidden flex flex-col space-y-4 px-4 pb-3">
         <nav className="flex flex-row pt-4 items-center justify-between ">
           <Searchbar isMobile={true} />
-          <Dropdown options={[]} title="security" isHeading={true} />
+          <Dropdown
+            options={dropDownNavSecurity}
+            title="security"
+            isHeading={true}
+          />
         </nav>
 
         <hr />
 
         <div className="flex flex-row justify-between items-center">
-          <h1 className="font-lexend text-p18 font-bold">Open Entries</h1>
-          <Link
-            to={`/security/closed`}
-            className="bg-slate-100 p-2 rounded-lg mx-1"
+          <h1 className="font-lexend text-p18 font-bold">
+            {filter?.isOpen ? "Open Entries" : "Closed Entries"}
+          </h1>
+          <label
+            htmlFor="toggleOpen"
+            className="bg-slate-100 p-2 rounded-lg mx-1 cursor-pointer "
           >
             <LuClipboardCheck style={{ fontSize: "24px" }} />
-          </Link>
+          </label>
+          <input
+            type="checkbox"
+            id="toggleOpen"
+            name="toggleOpen"
+            className="hidden"
+            checked={filter?.isOpen || false}
+            onChange={(e) => {
+              setFilter({ ...filter, isOpen: e.target.checked });
+            }}
+          />
         </div>
 
         <div className="shadow-lg bg-white border border-slate-200 px-4 py-2 rounded-lg">
