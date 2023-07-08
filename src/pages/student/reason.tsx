ce5bf3074934601cdc40ed7af14506559f342951
Styles <bcs_2021035@iiitm.ac.in>
2023-07-08 22:24:57 +0530

    added prompt for big screen

--- src/pages/student/reason.tsx
+++ src/pages/student/reason.tsx
@@ -23,18 +23,19 @@ const reason = () => {
       latitude: location?.latitude,
     };
 
+    console.log(requestObj)
+
     try {
       const response = await toast.promise(axios.post("/student/exit-request", requestObj), {
         loading: 'Verifying...',
         success: 'Successful',
-        error: (error) => ("Failed"),
+        error: (error) => (error.response.data || "Failed"),
       });
 
       console.log(response)
       navigate(`/student/success`);
     } catch (error: any) {
       console.log(error.response);
-      navigate(`/student/failure`);
     }
   };
 
@@ -85,6 +86,10 @@ const reason = () => {
           </div>
         </div>
       </form>
+      <div className="hidden xl:flex flex-col items-center justify-center h-screen">
+          <h1 className="font-bold text-sky-500 p-10 text-p20 shadow-card-shadow rounded-full border">Switch to a mobile device to view this page</h1>
+          <Link to={'/student/home'} className="font-medium text-p14 mt-5 underline underline-offset-2 transition hover:scale-110 text-sky-500">Go back</Link>
+      </div>
     </FormProvider>
   );
 };
