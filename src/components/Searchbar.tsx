f8c6e6d4edc2edb00537d538c3e28f53f74ab649
Styles <bcs_2021035@iiitm.ac.in>
2023-07-08 17:50:18 +0530

    Update Searchbar.tsx

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,7 +2,7 @@ import { Fragment, useState } from "react";
 import search from "../assets/icons/search.svg";
 import axios from "axios";
 import { useOutingStore } from "../store/store";
-import { Transition } from "@headlessui/react";
+import { Transition, Menu } from "@headlessui/react";
 
 type item = {
   hostel: string;
@@ -30,7 +30,7 @@ const Searchbar = (props: { isMobile: boolean }) => {
   console.log(dropdownItems);
 
   return (
-    <div className="relative">
+    <Menu as="div" className="relative inline-block text-left">
       <form
         className={`flex ${
           props.isMobile ? "bg-white" : "bg-slate-50"
@@ -46,13 +46,15 @@ const Searchbar = (props: { isMobile: boolean }) => {
           value={searchTerm}
           onChange={filterDropdown}
         />
-        <img
-          src={search}
-          alt="Search"
-          className="cursor-pointer inset-x-44 inset-y-2"
-        />
+        <Menu.Button>
+          <img
+            src={search}
+            alt="Search"
+            className="cursor-pointer inset-x-44 inset-y-2"
+          />
+        </Menu.Button>
       </form>
-      {/* <Transition
+      <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
@@ -60,29 +62,28 @@ const Searchbar = (props: { isMobile: boolean }) => {
         leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
-        show={true}
-      > */}
-      {dropdownItems && (
-        <div className="absolute w-full bg-white border border-gray-300 rounded-b-lg">
-          {dropdownItems.map((item) => {
-            return (
-              <div className="w-full hover:bg-gray-100">
-                <button
-                  key={item.username}
-                  onClick={() => {
-                    setFilter({ ...filter, username: item.username });
-                  }}
-                  className="block px-4 py-2 text-[14px]"
-                >
-                  {item.name}
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      )}
-      {/* </Transition> */}
-    </div>
+      >
+        {dropdownItems && (
+          <Menu.Items className="absolute w-full bg-white border border-gray-300 rounded-b-lg">
+            {dropdownItems.map((item) => {
+              return (
+                <div className="w-full hover:bg-gray-100">
+                  <button
+                    key={item.username}
+                    onClick={() => {
+                      setFilter({ ...filter, username: item.username });
+                    }}
+                    className="block px-4 py-2 text-[14px]"
+                  >
+                    {item.name}
+                  </button>
+                </div>
+              );
+            })}
+          </Menu.Items>
+        )}
+      </Transition>
+    </Menu>
   );
 };
 
