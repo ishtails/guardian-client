a38da64692ae00fa01364a2ae4a3e220ab00983c
Styles <bcs_2021035@iiitm.ac.in>
2023-07-11 03:06:01 +0530

    role based routing for ' / ' route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,14 +5,10 @@ import ChangePassword from "../pages/common/changePassword";
 import ForgotPassword_1 from "../pages/common/forgotPassword_1";
 import ForgotPassword_2 from "../pages/common/forgotPassword_2";
 import ForgotPassword_3 from "../pages/common/forgotPassword_3";
-import Register_1 from "../pages/student/register_1";
-import Register_2 from "../pages/student/register_2";
-import Register_3 from "../pages/student/register_3";
 import AdminDashboard from "../pages/admin/dashboard";
 import IndividualProfile from "../pages/admin/individual";
 import SecurityDashboard from "../pages/security/dashboard";
 import ClosedEntries from "../pages/security/closedEntries";
-import ClosingDialogue from "../components/CloseEntryModal";
 import Support from "../pages/common/support";
 import StudentDashboard from "../pages/student/dashboard";
 import StudentReport from "../pages/student/reports";
@@ -22,11 +18,13 @@ import FailedExit from "../pages/student/failure";
 import StudentUpdateInfo from "../pages/student/updateInfo";
 import LogOut from "../components/logOut";
 
+import Dashboard from "./HomeRoute";
+
 const mainRouter = () => {
   return (
     <Routes>
       {/* Common */}
-      <Route path="/" element={<LoginScreen/>}/>
+      <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/logout" element={<LogOut />} />
       <Route path="/forgotpass" element={<ForgotPassword_1 />} />
@@ -37,23 +35,20 @@ const mainRouter = () => {
 
       {/* Admin Routes */}
       <Route path="/admin/home" element={<AdminDashboard />} />
-      <Route path="/:username" element={<IndividualProfile />} />
+      <Route path="/profile/:username" element={<IndividualProfile />} />
 
       {/* Security Routes */}
-      <Route path="/security/home" element={<SecurityDashboard />}/> 
-      <Route path="/security/closed" element={<ClosedEntries />}/> 
-      <Route path="/security/sure?" element={<ClosingDialogue />}/> 
+      <Route path="/security/home" element={<SecurityDashboard />} />
+      <Route path="/security/closed" element={<ClosedEntries />} />
 
       {/* Student Routes */}
-      <Route path="/register/email" element={<Register_1/>} />
-      <Route path="/register/otp" element={<Register_2/>} />
-      <Route path="/register/pass" element={<Register_3/>} />
-      <Route path="/student/home" element={<StudentDashboard/>} />
-      <Route path="/student/report" element={<StudentReport/>} />
-      <Route path="/student/reason" element={<StudentReason/>} />
-      <Route path="/student/success" element={<SuccessfulExit/>} />
-      <Route path="/student/failure" element={<FailedExit/>} />
-      <Route path="/student/update" element={<StudentUpdateInfo/>} />
+
+      <Route path="/student/home" element={<StudentDashboard />} />
+      <Route path="/student/report" element={<StudentReport />} />
+      <Route path="/student/reason" element={<StudentReason />} />
+      <Route path="/student/success" element={<SuccessfulExit />} />
+      <Route path="/student/failure" element={<FailedExit />} />
+      <Route path="/student/update" element={<StudentUpdateInfo />} />
 
       <Route path="*" element={<Error404 />} />
     </Routes>
