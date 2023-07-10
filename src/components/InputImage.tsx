bdfdb03c02d1a4155b79aacddb10fb726804883e
Styles <bcs_2021035@iiitm.ac.in>
2023-07-11 05:01:01 +0530

    image preview logic

--- src/components/InputImage.tsx
+++ src/components/InputImage.tsx
@@ -1,13 +1,18 @@
 import toast from "react-hot-toast";
-import { useFormStore } from "../store/store";
+import { useFormStore, useUserStore } from "../store/store";
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
+import useFetchProfile from "../helpers/fetchUserHook";
 
 type Props = {
   label: string;
 };
 
 const InputImage = ({ label }: Props) => {
+  useFetchProfile("/profile");
+  const { user } = useUserStore();
+  console.log(user);
+
   const { formValues, setFormField } = useFormStore();
   const {
     register,
@@ -22,12 +27,18 @@ const InputImage = ({ label }: Props) => {
     if (value) {
       if (!allowedTypes.includes(value.type)) {
         setIsInvalid(true);
-        return toast.error("Invalid file type");
+        setFormField(fieldName, null);
+        return toast.error("Invalid file type", {
+          id:"invalid_type"
+        });
       }
 
       if (value.size >= 5 * 1024 * 1024) {
         setIsInvalid(true);
-        return toast.error("File size exceeds 5MB limit");
+        setFormField(fieldName, null);
+        return toast.error("File size exceeds 5MB limit", {
+          id:"size_exceed"
+        });
       }
 
       setFormField(fieldName, value);
@@ -41,45 +52,55 @@ const InputImage = ({ label }: Props) => {
       <label
         htmlFor={inputId}
         className={`${
-          errors[inputId] || isInvalid ? "border-red-300" : "border-slate-300"
+          errors[inputId] || isInvalid ? "border-red-300" : "border-sky-500"
         }
-        flex justify-center w-full h-32 px-4 transition bg-white border-2  border-dashed rounded-md appearance-none cursor-pointer hover:border-slate-400 focus:outline-none`}
+        flex justify-center px-4 transition bg-white border-2  rounded-md appearance-none cursor-pointer hover:border-slate-400 focus:outline-none w-full
+        ${!formValues[inputId] && user?.idCard ? "h-fit boder-solid" : "h-32  border-dashed"}`}
       >
-        <div className="flex flex-col justify-center items-center">
-          <span className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-slate-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              />
-            </svg>
-            <span className="font-medium text-gray-600">
-              {formValues[inputId] ? (
-                <span>{formValues[inputId].name}</span>
-              ) : (
-                <div>
-                  Drop files to Attach, or
-                  <span className="text-sky-500 underline ml-1">Browse</span>
-                </div>
-              )}
+        {!formValues[inputId] && user?.idCard ? (
+          <div className="flex flex-col items-center my-2">
+            <img className="mt-2 rounded-lg max-h-[24rem]" src={user?.idCard} />
+            <p className="text-red-600 ml-1 mt-2 font-medium">
+              Click to change image
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <span className="flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-slate-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                />
+              </svg>
+              <span className="font-medium text-gray-600">
+                {formValues[inputId] ? (
+                  <span>{formValues[inputId].name}</span>
+                ) : (
+                  <div>
+                    Drop files to Attach, or
+                    <span className="text-sky-500 underline ml-1">Browse</span>
+                  </div>
+                )}
+              </span>
             </span>
-          </span>
-          {formValues[inputId] ? (
-            <span className="text-sky-500 ml-1 font-medium mt-2">
-              Click to change
-            </span>
-          ) : (
-            <div />
-          )}
-        </div>
+            {formValues[inputId] ? (
+              <span className="text-sky-500 ml-1 font-medium mt-2">
+                Click to change
+              </span>
+            ) : (
+              <div />
+            )}
+          </div>
+        )}
       </label>
       <input
         id={inputId}
