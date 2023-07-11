0790b0d4e5251ca2d8ba7416fe1a5391941e2411
Styles <bcs_2021035@iiitm.ac.in>
2023-07-12 00:41:22 +0530

    optimized fetch user hook

--- src/helpers/fetchUserHook.ts
+++ src/helpers/fetchUserHook.ts
@@ -3,21 +3,26 @@ import { useEffect } from "react";
 import { useUserStore } from "../store/store";
 
 const useFetchProfile = (query: string) => {
-  const {user, setUser,  isLoading, setIsLoading} = useUserStore();
+  const { user, setUser, isLoading, setIsLoading } = useUserStore();
   useEffect(() => {
     if (!query) {
       setIsLoading(false);
       return;
-    };
+    }
 
     const fetchData = async () => {
       try {
         const response = await axios.get(query);
-        const outStatus = await axios.get('/student/outing-status');
-        if(outStatus.data === "outside"){
-          setUser({...response.data, isOutside:true})
-        } else{
-          setUser({...response.data, isOutside:false})
+
+        if (response.data.role === "student") {
+          const outStatus = await axios.get("/student/outing-status");
+          if (outStatus.data === "outside") {
+            setUser({ ...response.data, isOutside: true });
+          } else {
+            setUser({ ...response.data, isOutside: false });
+          }
+        } else {
+          setUser(response.data);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -28,7 +33,7 @@ const useFetchProfile = (query: string) => {
     fetchData();
   }, [query]);
 
-  return {user, isLoading};
+  return { user, isLoading };
 };
 
 export default useFetchProfile;
