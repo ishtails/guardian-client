b3c27b691e7b61820752c938eef98141815e7a6d
Styles <bcs_2021035@iiitm.ac.in>
2023-07-11 19:46:27 +0530

    Update forgotPassword_1.tsx

--- src/pages/common/forgotPassword_1.tsx
+++ src/pages/common/forgotPassword_1.tsx
@@ -19,59 +19,70 @@ const forgotPassForm_1 = () => {
     };
 
     try {
-      const response = await toast.promise(axios.post("/send-otp", requestObj), {
-        loading: 'Sending OTP...',
-        success: 'Successful',
-        error: (error) => error.response.data,
-      });
-      console.log(response)
+      const response = await toast.promise(
+        axios.post("/send-otp", requestObj),
+        {
+          loading: "Sending OTP...",
+          success: "Successful",
+          error: (error) => {
+            if (error.response.data === "OTP not expired") {
+              return "Wait before sending OTP...";
+            } else if (error.response.status === 422) {
+              return "Invalid email";
+            } else {
+              return "Server Error";
+            }
+          },
+        }
+      );
+      console.log(response);
       navigate(`/forgotpass_2`);
     } catch (error: any) {
-      console.log(error.response);
+      console.log(error);
     }
   };
 
   return (
-  <FormProvider {...methods}>
+    <FormProvider {...methods}>
       <form
         className="flex flex-col space-y-4"
         onSubmit={methods.handleSubmit(onSubmit)}
       >
-    {/* Header */}
-    <div className="">
-      <span className="font-lexend font-bold text-h36 sm:text-h32">
-        Forgot Password?
-      </span>
-      <p className="text-[#667085] text-h16 sm:text-h14">
-        Enter your Institute email to continue
-      </p>
-    </div>
+        {/* Header */}
+        <div className="">
+          <span className="font-lexend font-bold text-h36 sm:text-h32">
+            Forgot Password?
+          </span>
+          <p className="text-[#667085] text-h16 sm:text-h14">
+            Enter your Institute email to continue
+          </p>
+        </div>
 
-    {/* Input Fields */}
-    <InputField
-      label="forgotpass_Email"
-      placeholder="example@iiitm.ac.in"
-      isPassword={false}
-      validationRules={{ 
-        required: { value: true, message: "Required" },
-     }}
-    />
+        {/* Input Fields */}
+        <InputField
+          label="forgotpass_Email"
+          placeholder="example@iiitm.ac.in"
+          isPassword={false}
+          validationRules={{
+            required: { value: true, message: "Required" },
+          }}
+        />
 
-    {/* Submit */}
-    <div className="flex flex-col items-center space-y-2">
-      <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg hover:bg-sky-400 transition font-semibold">
-        Continue
-      </button>
-      <Link
-        to="/"
-        className="text-[#0EA5E9] font-medium hover:text-sky-600 transition hover:underline underline-offset-1 text-p14"
-      >
-        Back to Login
-      </Link>
-    </div>
-    </form>
+        {/* Submit */}
+        <div className="flex flex-col items-center space-y-2">
+          <button className="text-white text-h16 bg-[#0EA5E9] w-full p-2 rounded-lg hover:bg-sky-400 transition font-semibold">
+            Continue
+          </button>
+          <Link
+            to="/"
+            className="text-[#0EA5E9] font-medium hover:text-sky-600 transition hover:underline underline-offset-1 text-p14"
+          >
+            Back to Login
+          </Link>
+        </div>
+      </form>
     </FormProvider>
-);
+  );
 };
 
 const forgotPassword_1 = () => {
