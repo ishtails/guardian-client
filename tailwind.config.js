cd8fa3570957e8eaa5b665ac650c6d3b8fec1731
Styles <bcs_2021035@iiitm.ac.in>
2023-07-22 14:23:27 +0530

    removed unnecessary packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
     'node_modules/preline/dist/*.{js,jsx,ts,tsx}',
   ],
   theme: {
