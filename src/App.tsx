431dbbd2bcc3f6d69ea3d49b1001a20ab0155f65
Styles <bcs_2021035@iiitm.ac.in>
2023-07-28 10:59:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import MainRouter from "./routes/router";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "http://test-2-env.eba-cftxm3mp.ap-south-1.elasticbeanstalk.com/api";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
