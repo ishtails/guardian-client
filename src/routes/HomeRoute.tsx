b6680c97f8071825f417bb05f12b0b57e12f5965
Styles <bcs_2021035@iiitm.ac.in>
2023-07-28 17:25:31 +0530

    Update HomeRoute.tsx

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -8,7 +8,18 @@ const Dashboard = () => {
   const { user, isLoading } = useFetchProfile("/profile");
 
   if (isLoading) {
-    return <div>Home Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div
+          className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-info opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
   }
 
   if (user?.role === "admin") {
@@ -24,7 +35,7 @@ const Dashboard = () => {
   }
 
   if (!user?.role && !isLoading) {
-    return <LoginScreen />
+    return <LoginScreen />;
   }
 };
 
