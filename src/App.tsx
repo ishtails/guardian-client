66ddd2dce646e0418b511561f279323226321539
Styles <bcs_2021035@iiitm.ac.in>
2023-07-29 19:24:14 +0530

    fixed baseURL

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainRouter from "./routes/router";
 import { Toaster } from "react-hot-toast";
 
 if(import.meta.env.PROD){
-  axios.defaults.baseURL = "https://guardian-server.onrender.com";
+  axios.defaults.baseURL = "https://guardian-server.onrender.com/api";
 } else {
   axios.defaults.baseURL = "http://localhost:8000/api";
 }
