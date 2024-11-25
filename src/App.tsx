f0a731a61a714c51e2b6c164986842232b968899
Styles <kartik100100@gmail.com>
2024-11-26 00:19:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import axios from "axios";
 import MainRouter from "./routes/router";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "https://guardian-server.onrender.com/";
+if(import.meta.env.PROD){
+  axios.defaults.baseURL = "https://guardian-server.onrender.com/api";
+} else {
+  axios.defaults.baseURL = "http://localhost:8000/api";
+}
+
+axios.defaults.baseURL = "/";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
