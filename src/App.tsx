864263ed868264879ed3c419b27f38503fb4863b
Aneeka Mangal <aneekamangal@gmail.com>
2024-11-25 23:18:54 +0530

    updated be url

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import MainRouter from "./routes/router";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "https://guardian-server.onrender.com/";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
